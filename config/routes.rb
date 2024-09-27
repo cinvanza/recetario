cc1728a8745752b16cfb68eff7c10eb8ae9a51a1
cintia vanesa zavi <cintia_zavi@hotmail.com>
2024-09-27 14:28:42 -0300

    modelos, controlador y vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/edit'
+  get 'recipes/update'
+  get 'recipes/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
